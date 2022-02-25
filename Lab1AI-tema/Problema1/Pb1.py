0e345aa54ac8bda3e4afdbecce5431ef5b4e4848
DianaMaria09 <diana.ciupeiu09@gmail.com>
2022-02-25 10:48:09 +0200

    Cerinta

--- Lab1AI-tema/Problema1/Pb1.py
+++ Lab1AI-tema/Problema1/Pb1.py
@@ -1,5 +1,5 @@
 '''
-Created on 25 feb. 2022
-
-@author: Lenovo
+Sa se determine ultimul (din punct de vedere alfabetic) cuvant 
+care poate aparea intr-un text care conține mai multe cuvinte separate prin "" (spatiu). 
+De ex. ultimul (dpdv alfabetic) cuvânt din "Ana are mere rosii si galbene" este cuvântul "si".
 '''
