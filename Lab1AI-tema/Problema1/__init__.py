bd8d2e1f187af0d220dd24d8245a2f9b92a1949e
DianaMaria09 <diana.ciupeiu09@gmail.com>
2022-02-25 11:57:03 +0200

    Specificatii adaugate

--- Lab1AI-tema/Problema1/__init__.py
+++ Lab1AI-tema/Problema1/__init__.py
@@ -3,7 +3,12 @@ Sa se determine ultimul (din punct de vedere alfabetic) cuvant
 care poate aparea intr-un text care conține mai multe cuvinte separate prin "" (spatiu). 
 De ex. ultimul (dpdv alfabetic) cuvânt din "Ana are mere rosii si galbene" este cuvântul "si".
 '''
+
 def ultimCuvant(propozitie):
+    '''
+        propozitie - stringul introdus
+        returneaza ultimul cuvant (string) din propozitie, in ordine alfabetica
+    '''
     '''
         Complexitate: theta(n)- se parcurge liniar sirul de cuvinte
         Caz favorabil: theta(n)
